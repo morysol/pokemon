2689fce24db4b5c38c68fcac41505fa013032b2a
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-08-14 10:31:58 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// original
+// function App() {
+//   return <div className="App">aasasasas</div>;
+// }
+
+// export default App;
+
+export default class App extends Component {
+  state = { pokemon: null, loading: false };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((result) => result.json())
+      .then((pokemon) => this.setState({ pokemon }))
+      .finally(this.setState({ loading: false }));
+  }
+  render() {
+    const pokemon = this.state.pokemon;
+    return (
+      <div className="App">
+        {this.state.loading && <h1>Загрузка</h1>}
+        {pokemon && <div>{pokemon.name}</div>}
+      </div>
+    );
+  }
 }
-
-export default App;
